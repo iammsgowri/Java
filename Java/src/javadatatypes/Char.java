d880bcb99c53073162132ada76462cc01a66ca0a
Gowri <Gowri@192.168.1.3>
2026-02-13 11:15:02 +0530

    created java datatypes

---
+++ Java/src/javadatatypes/Char.java
@@ -0,0 +1,23 @@
+package javadatatypes;
+
+public class Char {
+
+	public static void main(String[] args) {
+
+		char character = 'G';
+
+		char number = '4';
+		System.out.println(number); // 4
+
+		// char number = '425';
+		// Invalid character constant
+
+		char numberchar = 100;
+		System.out.println(numberchar); // d
+
+		char currencySymbol = '₹';
+		System.out.println("Currency Symbol is: " + currencySymbol);
+
+	}
+
+}
