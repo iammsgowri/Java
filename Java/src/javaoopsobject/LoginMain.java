88e18920b5070d51ab567f978b6f25dcb30c927c
Gowri <Gowri@192.168.1.3>
2026-02-13 10:10:11 +0530

    created java login object

---
+++ Java/src/javaoopsobject/LoginMain.java
@@ -0,0 +1,15 @@
+package javaoopsobject;
+
+import javaoopsclass.Login;
+
+public class LoginMain {
+
+	public static void main(String[] args) {
+
+		Login user1 = new Login();
+		Login user2 = new Login();
+		Login user3 = new Login();
+
+	}
+
+}
